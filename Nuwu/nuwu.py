96c46cf87e80632a56c079e9f0ae46b844d31577
1clapch33k5 <51503087+iclapcheeks@users.noreply.github.com>
2019-12-27 20:06:51 -0800

    Update nuwu.py

--- Nuwu/nuwu.py
+++ Nuwu/nuwu.py
@@ -246,13 +246,13 @@ class Nuwu(commands.Cog):
         #Message Sending
 
     @commands.command()
-    async def lick(self, ctx):
+    async def lick(self, ctx, guild: discord.Guild):
         fauth = ctx.message.author.id
         auth = f'<@!{fauth}>'
         mem = ctx.message.content.split(' ')[1]
         msg = rand(self.lickm)
         #Variables
-        if mem in(guild.members):
+        if mem in(self.guild.members):
             await ctx.send(f'{mem} is not in the server, please use the correct syntax | [p]lick <member>')
         elif mem == auth:
             await ctx.send('H-how do you lick yourself 0-0...')
