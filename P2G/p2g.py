e83071fcdf58d263fe235d56e20ecd368c32e7a3
1clapch33k5 <51503087+iclapcheeks@users.noreply.github.com>
2019-12-29 05:42:31 -0800

    Update p2g.py

--- P2G/p2g.py
+++ P2G/p2g.py
@@ -15,4 +15,6 @@ class P2G(commands.Cog):
             await ctx.send(':x: INCORRECT SYNTAX | [p]p2g <ipv4>')
         if ipv4:
             geocalc = geocoder.ip(f'{ipv4}')
-            await ctx.send(f':white_check_mark: SUCCESSFULY CALCULATED GEOLOCATION VALUE | {geocalc.latlng[1]} |')
+            geo1 = geocalc.latlng[0]
+            geo2 = geocalc.latlng[1]
+            await ctx.send(f':white_check_mark: SUCCESSFULY CALCULATED GEOLOCATION VALUE | {geo1}, {geo2} |')
