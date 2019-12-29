0bfacaa9095b5d4c5e0de0ef610b932c2879a148
1clapch33k5 <51503087+iclapcheeks@users.noreply.github.com>
2019-12-29 05:51:12 -0800

    Update p2g.py

--- P2G/p2g.py
+++ P2G/p2g.py
@@ -13,10 +13,18 @@ class P2G(commands.Cog):
     async def p2g(self, ctx, ipv4):
         if ipv4 is None:
             await ctx.send(':x: INCORRECT SYNTAX | [p]p2g <ipv4>')
+            #Checking no IPV4
         if ipv4:
             geocalc = geocoder.ip(f'{ipv4}')
             geo1 = geocalc.latlng[0]
             geo2 = geocalc.latlng[1]
-            mgmsg = f'{geo1}{geo2}'
+            #Getting Latitude/Longitude
+
+            mgmsg = f':white_check_mark: FOUND GEOLOCATION | {geo1}, {geo2}'
+            mgmap = f'https://www.google.com/maps/search/{geo1},+{geo2}'
+            #Message + Creating URL
+
             geobed = discord.Embed(description=mgmsg,color=discord.Color(0xff4040))
+            geobed.set_image(url=mgmap)
             await ctx.send(embed=geobed)
+            #Creating & Sending Embed
