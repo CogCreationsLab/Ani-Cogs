82f1d0273adff8def68fb1e986d4915c87b26d6a
1clapch33k5 <51503087+iclapcheeks@users.noreply.github.com>
2019-12-29 20:19:18 -0800

    Update p2g.py

--- P2G/p2g.py
+++ P2G/p2g.py
@@ -12,28 +12,27 @@ class P2G(commands.Cog):
     @commands.command()
     async def p2g(self, ctx, ipv4):
         geocalc = geocoder.ip(f'{ipv4}')
+        geolat = geocalc.lat
+        geolng = geocalc.lng
+        #Getting Latitude/Longitude
+
+        mgmsg = f':white_check_mark: FOUND GEOLOCATION | {geolat}, {geolng}'
+        mgmap = f'https://www.google.com/maps/search/{geolat},+{geolng}'
+        #Message + Creating URL
 
         if ipv4 is None:
             await ctx.send(':x: INCORRECT SYNTAX | [p]p2g <ipv4>')
             #Checking for IPV4
 
-        if geo1 is None:
+        if geolat is None:
             await ctx.send(':x: INCORRECT SYNTAX | [p]p2g <ipv4>')
             #Checking for Latitude
 
-        if geo2 is None:
+        if geolng is None:
             await ctx.send(':x: INCORRECT SYNTAX | [p]p2g <ipv4>')
-            #Checking for Longitude/Latitude
+            #Checking for Longitude
 
         if ipv4:
-            geo1 = geocalc.latlng[0]
-            geo2 = geocalc.latlng[1]
-            #Getting Latitude/Longitude
-            
-            mgmsg = f':white_check_mark: FOUND GEOLOCATION | {geo1}, {geo2}'
-            mgmap = f'https://www.google.com/maps/search/{geo1},+{geo2}'
-            #Message + Creating URL
-
             geobed = discord.Embed(description=mgmsg,color=discord.Color(0xff4040))
             geobed.set_image(url=mgmap)
             await ctx.send(embed=geobed)
