77a7c335b31e758253b6c58273bf8356166ccb45
1clapch33k5 <51503087+iclapcheeks@users.noreply.github.com>
2019-12-29 20:06:22 -0800

    Update p2g.py

--- P2G/p2g.py
+++ P2G/p2g.py
@@ -12,8 +12,6 @@ class P2G(commands.Cog):
     @commands.command()
     async def p2g(self, ctx, ipv4):
         geocalc = geocoder.ip(f'{ipv4}')
-        geo1 = geocalc.latlng[0]
-        geo2 = geocalc.latlng[1]
         #Getting Latitude/Longitude
 
         mgmsg = f':white_check_mark: FOUND GEOLOCATION | {geo1}, {geo2}'
@@ -33,6 +31,8 @@ class P2G(commands.Cog):
             #Checking for Longitude/Latitude
 
         if ipv4:
+            geo1 = geocalc.latlng[0]
+            geo2 = geocalc.latlng[1]
             geobed = discord.Embed(description=mgmsg,color=discord.Color(0xff4040))
             geobed.set_image(url=mgmap)
             await ctx.send(embed=geobed)
