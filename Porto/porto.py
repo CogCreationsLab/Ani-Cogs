42e876c4af0061df6ead2052543d68af7475aacc
1clapch33k5 <51503087+iclapcheeks@users.noreply.github.com>
2019-12-30 21:16:44 -0800

    Update porto.py

--- Porto/porto.py
+++ Porto/porto.py
@@ -24,7 +24,7 @@ class Porto(commands.Cog):
         await ctx.send(f'*[+] SCANNING **{ipv4}** FOR OPEN PORTS[+]*')
         #Confirmation message
 
-        for port in range(80,65500):
+        for port in range(80,100):
             if sock.connect_ex((ipv4, port)):
                 open_ports.append(port)
                 #Adding open ports to list
