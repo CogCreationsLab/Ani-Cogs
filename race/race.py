56f8c47112143f8a4aa1929f2e81bb4d908314fc
Cog Creations Lab <59899391+CogCreationsLab@users.noreply.github.com>
2020-01-15 01:14:44 -0500

    Update race.py

--- race/race.py
+++ race/race.py
@@ -258,8 +258,8 @@ class Race(commands.Cog):
             Racers are randomly selected from a list of animals with
             different attributes.
         """
-        if mode.lower() not in ('zoo', 'normal'):
-            return await ctx.send("Must select either `zoo` or `normal` as a mode.")
+        if mode.lower() not in ('zoo', 'normal', `clashroyale`):
+            return await ctx.send("Must select either `zoo`, `normal`, or `clashroyale` as a mode.")
 
         await self.db.guild(ctx.guild).Mode.set(mode.lower())
         await ctx.send(f"Mode changed to {mode.lower()}")
@@ -471,6 +471,20 @@ class Race(commands.Cog):
                 players.append((Animal(":turtle:", "slow"), ctx.bot.user))
         return players
 
+    async def _game_setup(self, ctx):
+        mode = await self.db.guild(ctx.guild).Mode()
+        users = self.players
+        if mode == 'clashroyale':
+            players = [(CRChars(*random.choice(CRracers)), user) for user in users]
+            if len(players) == 1:
+                players.append((CRChars(*random.choice(CRracers)), ctx.bot.user))
+        else:
+            players = [(Animal(":turtle:", "slow"), user) for user in users]
+            if len(players) == 1:
+                players.append((Animal(":turtle:", "slow"), ctx.bot.user))
+        return players
+
+                       
     async def run_game(self, ctx):
         players = await self._game_setup(ctx)
         setup = "\u200b\n" + '\n'.join(f":carrot: **{animal.current}** 🏁"  
