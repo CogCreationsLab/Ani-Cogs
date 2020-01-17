c9baf6afdb00a128e0d3e83c2b30e683fb22055c
Xzadik| <53629779+XzadikApple@users.noreply.github.com>
2020-01-17 01:41:05 -0500

    Update race.py

--- race/race.py
+++ race/race.py
@@ -87,8 +87,8 @@ class Race(commands.Cog):
             wait = await self.db.guild(ctx.guild).Wait()
             current = await self.db.guild(ctx.guild).Games_Played()
             await self.db.guild(ctx.guild).Games_Played.set(current + 1)
-            await ctx.send(f"🚩 {ctx.author.mention} has started a race!\nType `b!race enter` "
-                        f"to join the race! 🚩\n          The {} will begin in "
+            await ctx.send(f"🚩 {ctx.author.mention} has started a race!\nType `b!race enter`"
+                        f"to join the race! 🚩\nThe {} will begin in "
                         f"{wait} seconds!").format(author.mention, ctx.prefix, ' ' * 23, raceRole.mention, wait)
             await self.bot.edit_role(server, raceRole, mentionable=False)
             await asyncio.sleep(wait)
